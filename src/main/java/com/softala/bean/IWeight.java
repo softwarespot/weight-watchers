95d933a41a29f134e86855d4d392a1e222d44c4f
softwarespot <softwarespotapps@gmail.com>
2015-10-27 23:39:11 +0200

    Added interface

---
+++ src/main/java/com/softala/bean/IWeight.java
@@ -0,0 +1,23 @@
+package com.softala.bean;
+
+import java.util.Date;
+
+public interface IWeight {
+	
+	public abstract int getId();
+	
+	public abstract Date getTime();
+	
+	public abstract String getUsername();
+	
+	public abstract float getValue();
+	
+	public abstract void setId(int id);
+	
+	public abstract void setTime(Date time);
+	
+	public abstract void setUsername(String username);	
+	
+	public abstract void setValue(float value);
+
+}
