3cb5453d83a10b62cac184b58057c2e6735e17d5
softwarespot <softwarespotapps@gmail.com>
2015-10-27 18:55:15 +0200

    Using NOW()

--- database/insert_test_data_mysql.sql
+++ database/insert_test_data_mysql.sql
@@ -1,6 +1,4 @@
-INSERT INTO Weights
-	(value, time, username)
-VALUES
-	(71, 2015-10-13 17:42:20, 'Jukka'),
-	(68, 2015-10-13 17:42:27, 'SoftwareSpot'),
-	(50, 2015-10-13 17:32:27, 'InkaH');
+INSERT INTO Weights (value, time, username) 
+	VALUES 	(71, NOW(), 'Jukka'),
+			(68, NOW(), 'SoftwareSpot'),
+			(50, NOW(), 'InkaH');
