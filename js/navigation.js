91f2fe3a2bc7431803903fe0a68c1229204e51d6
softwarespot <softwarespotapps@gmail.com>
2015-10-31 00:15:17 +0200

    Fixed traversal of DOM

--- js/navigation.js
+++ js/navigation.js
@@ -157,7 +157,7 @@ App.navigation = (function (window, document, $, core, undefined) {
      */
     function _cacheDom() {
         _$body = $('html, body');
-        _$navigationLinks = $('header').find('a');
+        _$navigationLinks = _$body.find('header').find('a');
     }
 
     /**
